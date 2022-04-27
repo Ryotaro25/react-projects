3541e3e5e0717c5804db1c85dfdbd1f4783583de
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2022-04-27 22:40:10 +0900

    finish submenu

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-
+import { useGlobalConText } from './context'
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalConText();
+  
+  return <section className='hero' onMouseOver={closeSubmenu}>
+    <div className="hero-center">
+      <article className='hero-info'>
+        <h1>Patments Infrastructure for the internet</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempora omnis cumque, consequatur molestiae quas cupiditate. Mollitia, laborum accusamus pariatur, ipsam doloremque architecto eaque enim nisi soluta deserunt assumenda nihil.</p>
+        <button className='btn'>Start now</button>
+      </article>
+      <article className='hero-images'>
+        <img src={phoneImg} alt="phone" className='phone-img' />
+      </article>
+    </div>
+  </section>
 }
 
 export default Hero
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,48 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({page: '', links: []});
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = (text, cordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page);
+        setLocation(cordinates);
+        setSubmenuOpen(true);
+    }
+
+    const closeSubmenu = () => {
+        setSubmenuOpen(false);
+    }
+
+
+    return <AppContext.Provider value={{
+        isSubmenuOpen, 
+        isSidebarOpen,
+        openSidebar, 
+        closeSidebar, 
+        openSubmenu, 
+        closeSubmenu,
+        location,
+        page
+    }}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalConText = () => {
+    return useContext(AppContext);
+}
